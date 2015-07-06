30fc1236bb0f3395a5ea17b8b03c42a7ed27c507
Thinh <lethinh0510@gmail.com>
2015-07-06 11:26:15 +0700

    main

---
+++ app/src/main/java/com/lethinh/salecard/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.lethinh.salecard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Thinh on 06/07/2015.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
